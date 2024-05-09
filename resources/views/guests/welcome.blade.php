3b679b25d8d6f6d36ad7d7fdd782c1b2d9ba54bb
emamoldo <moldovanemanuel92.em@gmail.com>
2024-05-09 18:13:15 +0200

    Add some style: Add the cards with boostrap

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -5,13 +5,41 @@
 <div class="container">
     <h1>Movies:</h1>
 
-    @forelse($movies as $movie)
-        <h4>{{$movie->title}}</h4>
-        <p>{{$movie->nationality}}</p>
-    @empty
-        <h1>No Movies Here</h1>
-    @endforelse
-</div>
+    <div class="container text-center">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+            @forelse($movies as $movie)
+                <div class="col">
+                    <div class="card py-4 my-4">
+                        <div class="card-body">
+                            <div class="col">
+                                <strong>Title:
+                                    <h6>{{$movie->title}}</h6>
+                                </strong>
+
+                                <strong> Original Title:
+                                    <h5>{{$movie->original_title}}</h5>
+                                </strong>
 
 
-@endsection
+                                <strong> Nationality:
+                                    <p>{{$movie->nationality}}</p>
+                                </strong>
+
+                                <strong> Date of Release:
+                                    <p>{{$movie->date}}</p>
+                                </strong>
+
+                                <strong> Vote:
+                                    <p>{{$movie->vote}}</p>
+                                </strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <h1>No Movies Here</h1>
+            @endforelse
+        </div>
+    </div>
+
+    @endsection
